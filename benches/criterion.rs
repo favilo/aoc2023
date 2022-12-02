04c9c1a3429dc9633829e800a4a3fdb8c92fea7d
Kevin Oberlies <favilo@gmail.com>
2022-12-01 22:44:16 -0800

    Adding benchmarks

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -7,12 +7,12 @@ use pprof::{criterion::Output, flamegraph::Options};
 
 macro_rules! days {
     ($day:ident) => {
-        use aoc2021::$day;
+        use aoc2022::$day;
 
         fn $day(c: &mut Criterion) {
             let mut group = c.benchmark_group(stringify!($day));
             let input =
-                read_to_string(format!("input/2021/day{:02}.txt", $day::Day::day())).unwrap();
+                read_to_string(format!("input/2022/day{:02}.txt", $day::Day::day())).unwrap();
             group.bench_function("get_input", |b| {
                 b.iter(|| $day::Day::get_input(&input))
             });
@@ -43,7 +43,7 @@ macro_rules! benches {
     };
 }
 
-benches!(day01);
+benches!(day01, day02,);
 
 fn custom() -> Criterion {
     let mut options = Options::default();
