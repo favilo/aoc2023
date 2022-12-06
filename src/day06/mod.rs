69c3ab6c4be3c96a4005209390cdd673bb201c32
Kevin Oberlies <favilo@gmail.com>
2022-12-06 11:00:17 -0800

    Fixed a bug

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -5,32 +5,32 @@ use crate::Runner;
 pub struct Day;
 
 impl Runner for Day {
-    type Input = Vec<u8>;
+    type Input = String;
 
     fn day() -> usize {
         6
     }
 
     fn get_input(input: &str) -> Result<Self::Input> {
-        Ok(Vec::from(input.as_bytes()))
+        Ok(input.to_owned())
     }
 
     fn part1(input: &Self::Input) -> Result<usize> {
-        Ok(get_index(input, 4))
+        Ok(get_index(input.as_bytes(), 4))
     }
 
     fn part2(input: &Self::Input) -> Result<usize> {
-        Ok(get_index(input, 14))
+        Ok(get_index(input.as_bytes(), 14))
     }
 }
 
-fn get_index(input: &Vec<u8>, window_size: usize) -> usize {
+fn get_index(input: &[u8], window_size: usize) -> usize {
     // Fill the set with the first w - 1 items
     // The XOR will remove an item if it is in there twice.
     let mut set = input
         .into_iter()
         .take(window_size - 1)
-        .fold(0_u64, |a, &c| a ^ (c - b'a') as u64);
+        .fold(0_u64, |a, &c| a ^ (1 << (c - b'a')));
     input
         .windows(window_size)
         .enumerate()
