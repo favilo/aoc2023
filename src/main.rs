32804ecd2659a0da70f241cbfbd3b1aaa7d64f18
Kevin Oberlies <favilo@gmail.com>
2022-12-08 20:55:25 -0800

    Add the ability to panic on allocations.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::AtomicBool;
+
 use clap::{ArgAction, Parser};
 use color_eyre::Result;
 use fern::colors::{Color, ColoredLevelConfig};
@@ -6,6 +8,8 @@ use tracking_allocator::{AllocationGroupId, AllocationRegistry, AllocationTracke
 #[global_allocator]
 static GLOBAL: tracking_allocator::Allocator<std::alloc::System> =
     tracking_allocator::Allocator::system();
+
+static PANIC_ON_ALLOCATE: AtomicBool = AtomicBool::new(false);
 
 // #[global_allocator]
 // static GLOBAL: Allocator<System> = Allocator::system();
@@ -37,6 +41,10 @@ impl AllocationTracker for StdoutTracker {
         source_group_id: AllocationGroupId,
         current_group_id: AllocationGroupId,
     ) {
+        if PANIC_ON_ALLOCATE.load(std::sync::atomic::Ordering::SeqCst) {
+            panic!("We've allocated!");
+        }
+
         log::debug!(
             "deallocation -> addr=0x{:0x} object_size={} wrapped_size={} source_group_id={:?} current_group_id={:?}",
             addr, object_size, wrapped_size, source_group_id, current_group_id
@@ -74,6 +82,9 @@ struct Args {
     days: Vec<usize>,
     #[arg(short = 't', long = "track")]
     track_allocations: bool,
+
+    #[arg(short = 'p', long = "panic")]
+    panic: bool,
 }
 
 fn main() -> Result<()> {
@@ -82,6 +93,9 @@ fn main() -> Result<()> {
     setup_logger()?;
     AllocationRegistry::set_global_tracker(StdoutTracker)
         .expect("no other global tracker should be set yet");
+    if args.panic {
+        PANIC_ON_ALLOCATE.store(true, std::sync::atomic::Ordering::SeqCst);
+    }
 
     let time = aoc2022::run(days, args.track_allocations)?;
     log::info!("Total Time: {:?}", time);
